230a1da90946c0c489f4d3dd01a7af27c2ce8f66
SWGfLWeb Team <webteam@swgfl.org.uk>
2017-10-17 10:32:07 +0100

    Fixed bug in empty/test.js where insertAdjacentHTML was cased incorrectly.

--- src/attributes/empty/test.js
+++ src/attributes/empty/test.js
@@ -2,9 +2,9 @@ QUnit.module("Attributes");
 
 QUnit.test("$.fn.empty", function (assert) {
 	var main = document.getElementsByClassName("main")[0];
-	main.insertAdjacentHtml("beforeEnd", "<span class=\"empty\">Test</span>");
+	main.insertAdjacentHTML("beforeEnd", "<span class=\"empty\">Test</span>");
 	var empty = $(".empty");
-	
+
 	assert.deepEqual(empty.empty(), empty, "Returns itself on empty");
 	assert.equal(empty.get(0).innerHTML, "", "Can empty node");
 });
