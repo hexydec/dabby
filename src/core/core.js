2c7cf471821ba62740a94bd4d4abea6e695369d0
Hexydec <will@hexydec.com>
2018-10-27 21:23:10 +0100

    Missing include in core.js.

--- src/core/core.js
+++ src/core/core.js
@@ -1,6 +1,7 @@
 import $ from "./dabby/dabby.js";
 
 import "./each/each.js";
+import "../attributes/attr/attr.js";
 import "../utils/each/each.js";
 import "../utils/iswindow/iswindow.js";
 import "../utils/isfunction/isfunction.js";
