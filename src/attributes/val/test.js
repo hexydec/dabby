98d51e352d1ca43dc78f827dd8623dfa2edd4799
Hexydec <will@hexydec.com>
2019-09-13 16:48:27 +0100

    Added tests for select boxes in $.fn.val().

--- src/attributes/val/test.js
+++ src/attributes/val/test.js
@@ -10,7 +10,12 @@ QUnit.test("$.fn.val", function (assert) {
 		opt,
 		radio = $("<input>", {type: "radio", name: "radio", value: "radio1"}).add(
 			$("<input>", {type: "radio", name: "radio", value: "radio2"})
-		);
+		),
+		select = $("<select>")
+			.append($("<option>", {text: "Select item"}))
+			.append($("<option>", {value: 1, text: "Item 1"}))
+			.append($("<option>", {value: 2, text: "Item 2"}))
+			.append($("<option>", {text: "3"}));
 
 	assert.equal(obj.val(), "test", "Can read value");
 	assert.deepEqual(obj.val("new value"), obj, "Returns self when setting value");
@@ -32,6 +37,11 @@ QUnit.test("$.fn.val", function (assert) {
 
 	assert.equal(radio.val(), "radio1", "Can retrieve value of radio box");
 	assert.equal(radio.val(["radio2"]), radio, "Can set value of radio box");
-	assert.equal(radio.filter(":checked").val(), "radio2", "Can get value of radio box");
+	assert.equal(radio.filter(":checked").val(), "radio2", "Can retrieve value of radio box");
 
+	assert.equal("Select item", select.val(), "Can retrieve value of select box");
+	select.val(2);
+	assert.equal("2", select.val(), "Can set value of select box");
+	select.val(3);
+	assert.equal("3", select.val(), "Can set value of select box that has no value attribute");
 });
