117602b0cbeef86bf5d2f61706889bd7b6d46634
hexydec <will@hexydec.com>
2020-10-03 18:32:35 +0100

    Changed single quotes to doubles in isplainobject.js.

--- src/utils/isplainobject/isplainobject.js
+++ src/utils/isplainobject/isplainobject.js
@@ -6,13 +6,13 @@ $.isPlainObject = obj => {
 	if (obj !== null && typeof obj === "object") {
 
 		// If Object.getPrototypeOf supported, use it
-	    if (typeof Object.getPrototypeOf === 'function') {
+	    if (typeof Object.getPrototypeOf === "function") {
 			let proto = Object.getPrototypeOf(obj);
 			return proto === Object.prototype || proto === null;
-	    }
+		}
 
-	    // Otherwise, use internal class
-	    // This should be reliable as if getPrototypeOf not supported, is pre-ES5
+		// Otherwise, use internal class
+		// This should be reliable as if getPrototypeOf not supported, is pre-ES5
 		return Object.prototype.toString.call(obj) === "[object Object]";
  	}
 
