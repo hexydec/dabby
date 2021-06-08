55a1afcd3c3185b3b9efdd616849e75a2e2518a7
Hexydec <will@hexydec.com>
2021-06-08 22:36:13 +0100

    Fixed bug in $.fn.empty() where a while statement had all its commands inside the condition, but no commands in the block.

--- src/manipulation/empty/empty.js
+++ src/manipulation/empty/empty.js
@@ -3,7 +3,9 @@ import $ from "../../core/core.js";
 $.fn.empty = function () {
 	let i = this.length;
 	while (i--) {
-		while (this[i].firstChild && this[i].removeChild(this[i].firstChild));
+		while (this[i].firstChild) {
+			this[i].removeChild(this[i].firstChild);
+		}
 	}
 	return this;
 };
