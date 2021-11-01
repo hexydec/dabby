b5713f9ca576d2d1ab3c6df25b6f17b17eca3e14
Hexydec <will@hexydec.com>
2021-11-01 21:29:20 +0000

    Fixed issue in camelise.js where it shouldn't change the case of CSS variables. Added tests.

--- src/internal/camelise/camelise.js
+++ src/internal/camelise/camelise.js
@@ -1,1 +1,1 @@
-export default prop => prop.replace(/-([\w])/g, (text, letter) => letter.toUpperCase()); // matches underscore too but you shouldn't do that anyway
+export default prop => prop.indexOf("--") === 0 ? prop : prop.replace(/-([\w])/g, (text, letter) => letter.toUpperCase()); // matches underscore too but you shouldn't do that anyway
--- src/internal/setcss/test.js
+++ src/internal/setcss/test.js
@@ -1,5 +1,4 @@
 import $ from "../../../dist/dabby.js";
-
 import setCss from "./setcss.js";
 
 QUnit.module("Internal");
@@ -13,7 +12,7 @@ QUnit.test("setCss", function (assert) {
 			"border-left-color": "rgb(255, 0, 0)",
 			"border-left-style": "solid",
 			"border-left-width": "1px"
-		};;
+		};
 
 	rmain.style.cssText = "";
 	assert.deepEqual(setCss(main, "border-left-color", "red"), main, "Returns Dabby object when CSS is set");
@@ -34,4 +33,6 @@ QUnit.test("setCss", function (assert) {
 	assert.equal(rmain.style.borderLeftColor, "green", "Can set CSS property through a callback function");
 	setCss(main, {borderLeftColor: "", borderLeftStyle: "", borderLeftWidth: ""});
 	assert.equal(rmain.style.cssText, "", "Can remove CSS properties");
+	setCss(main, {"--css-variable": "test"});
+	assert.equal(rmain.style["--css-variable"], "test", "Can set CSS variable");
 });
