09006379e423377535d15692e30836a1f990d428
Hexydec <will@hexydec.com>
2021-12-14 17:10:59 +0000

    Fixed bug in $.fn.remove() where if a node was not attached to a parent, an error was produced. Updated $.fn.remove() to support removing the attached events from the removed nodes. Added tests.

--- src/manipulation/remove/remove.js
+++ src/manipulation/remove/remove.js
@@ -1,20 +1,25 @@
 import $ from "../../core/core.js";
+import "../../events/off/off.js";
 import filterNodes from "../../internal/filternodes/filternodes.js";
 
-["remove", "detach"].forEach((func, f) => {
+["detach", "remove"].forEach((func, f) => {
 	$.fn[func] = function (selector) {
 		let i = this.length,
-			nodes = [],
-			obj = [];
+			nodes = [];
 
 		// detach selected nodes
 		while (i--) {
 			if (!selector || filterNodes(this[i], selector).length) {
-				nodes.push(this[i].parentNode.removeChild(this[i]));
+
+				// remove data from removed nodes
+				if (f) {
+					$(this[i]).off();
+				}
+				nodes.push(this[i].parentNode ? this[i].parentNode.removeChild(this[i]) : this[i]); // only remove it attached to something
 			}
 		}
 
 		// create a new dabby object to return
-		return f ? $(nodes) : this;
+		return f ? this : $(nodes);
 	};
 });
--- src/manipulation/remove/test.js
+++ src/manipulation/remove/test.js
@@ -5,11 +5,20 @@ QUnit.module("Manipulation");
 QUnit.test("$.fn.remove", function (assert) {
 	var test = document.getElementsByClassName("test")[0];
 	test.innerHTML = '<div class="testtemp"><div></div></div><div class="testtemp2"></div>';
-	var obj = $(".testtemp, .testtemp2"),
+	var clicked = 0,
+		obj = $(".testtemp, .testtemp2").on("click", () => {clicked++;console.log("clicked "+clicked);}),
 		robj = Array.from(document.querySelectorAll(".testtemp, .testtemp2"));
 
 	assert.deepEqual(obj.remove(".testtemp2").get(), robj, "Returns reduced set when removed");
 	assert.ok(document.getElementsByClassName("testtemp2").length === 0, "Removed item from DOM");
+	assert.ok(obj.length === 2, "Dabby object contains correct number of elements");
+	obj.click();
+	console.log(obj, clicked);
+	assert.ok(clicked === 1, "Removed attached events");
+
+	// test detached node
+	var node = $("<div>");
+	assert.deepEqual(node.remove(), node, "No error when attempting to remove a node that is not attached");
 });
 
 QUnit.test("$.fn.detach", function (assert) {
