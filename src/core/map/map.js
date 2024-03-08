d57ba8395b65d981cd8b3f7c07d26a67a104c58a
Hexydec <will@hexydec.com>
2024-03-08 14:41:01 +0000

    Fixed issue in `$.fn.map()` where in jQuery, a jQuery object can contain an array, as well as a set of nodes, whereas in Dabby it can only be nodes. Therefore this method, whilst it should return a Dabby object, but was just returning an array, it must also restrict the result of the callback to just being either nodes or another Dabby object, which are now all merged together into the output.

--- src/core/map/map.js
+++ src/core/map/map.js
@@ -1,4 +1,5 @@
 import {Dabby} from "../../core/dabby/dabby.js";
+import "../../traversal/add/add.js";
 
 /**
  * A callback to receive nodes from a Dabby object
@@ -6,22 +7,22 @@ import {Dabby} from "../../core/dabby/dabby.js";
  * @callback mapCallback
  * @param {number} [index] The index of the node in the Dabby object 
  * @param {Node} [element] A Node object
- * @returns {any} Any value can be returned from each call
+ * @returns {node|node[]|Dabby} A node, array of nodes, or a Dabby object
  */
 
 /**
  * Run a custom callback function on each item in a Dabby collection
  * 
  * @param {mapCallback} callback - A callback to process each node in the Dabby object
- * @returns {any[]} An array of values returned from each callback on a node
+ * @returns {Dabby} A Dabby object containing the mapped nodes
  */
 const map = function (callback) {
 	let len = this.length,
-		values = [],
+		values = $(),
 		i = 0;
 
 	for (; i < len; i++) {
-		values.push(callback.call(this[i], i, this[i]));
+		values.add($(callback.call(this[i], i, this[i])));
 	}
 	return values;
 };
